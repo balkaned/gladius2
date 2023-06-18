b1d0fb7acf24b3303b130477660c752008cc5d39
balkaned <gitbalkaned@gmail.com>
2023-06-17 19:07:45 -0500

    Initial Commit

---
+++ src/main/java/com/balkaned/gladius/GladiusApplication.java
@@ -0,0 +1,13 @@
+package com.balkaned.gladius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GladiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GladiusApplication.class, args);
+	}
+}
+
