1732f5356f3fb7b53cf8e3dbf5b618d3a749466a
Javier Luján <javier.lujan.121@outlook.es>
2023-10-23 14:56:25 -0500

    Nuevo Concepto

--- src/main/java/com/balkaned/gladius/controllers/ConceptoController.java
+++ src/main/java/com/balkaned/gladius/controllers/ConceptoController.java
@@ -59,4 +59,38 @@ public class ConceptoController {
 
         return new ModelAndView("public/gladius/confPlanilla/conceptos/listConceptos");
     }
+
+    @RequestMapping("/nuevoConcepto")
+    public ModelAndView nuevoConcepto(
+            ModelMap model,
+            HttpServletRequest request
+    ) {
+        log.info("/nuevoConcepto");
+
+        String user = (String) request.getSession().getAttribute("user");
+
+        if(request.getSession().getAttribute("user")==null) {
+            return new ModelAndView("redirect:/login2");
+        }
+
+        String usuario = (String) request.getSession().getAttribute("user");
+        String idusuario = (String) request.getSession().getAttribute("idUser");
+        String email = (String) request.getSession().getAttribute("email");
+        String firstCharacter = (String) request.getSession().getAttribute("firstCharacter");
+        Integer idCompania = (Integer) request.getSession().getAttribute("idCompania");
+        String nombreComp = (String) request.getSession().getAttribute("nombrecomp");
+        String rucComp = (String) request.getSession().getAttribute("ruccomp");
+        String urlLogo = (String) request.getSession().getAttribute("urlLogo");
+
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("idusuario",idusuario);
+        model.addAttribute("email",email);
+        model.addAttribute("firstCharacter",firstCharacter);
+        model.addAttribute("nombreComp", nombreComp);
+        model.addAttribute("rucComp",rucComp);
+        model.addAttribute("idComp",idCompania);
+        model.addAttribute("urlLogo",urlLogo);
+
+        return new ModelAndView("public/gladius/confPlanilla/conceptos/nuevoConcepto");
+    }
 }
