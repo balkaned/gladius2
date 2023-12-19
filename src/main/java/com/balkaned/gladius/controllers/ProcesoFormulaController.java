7d835a238423c76b417b8b89d631e083888f6eb9
Javier Luján <javier.lujan.121@outlook.es>
2023-12-19 18:31:14 -0500

    Actualización procesos y form con sessionattribute

--- src/main/java/com/balkaned/gladius/controllers/ProcesoFormulaController.java
+++ src/main/java/com/balkaned/gladius/controllers/ProcesoFormulaController.java
@@ -4,7 +4,7 @@ import com.balkaned.gladius.beans.FormulaXConcepto;
 import com.balkaned.gladius.beans.Proceso;
 import com.balkaned.gladius.beans.ProcesoForm;
 import com.balkaned.gladius.services.ProcesoFormulaService;
-import lombok.extern.log4j.Log4j2;
+import com.balkaned.gladius.servicesImpl.Sessionattributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,52 +13,27 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.logging.Logger;
 
-@Log4j2
 @RestController
 public class ProcesoFormulaController {
 
 	@Autowired
 	ProcesoFormulaService service;
 
+	@Autowired
+	Sessionattributes sessionattributes;
+
+	static Logger logger = Logger.getLogger(AreaController.class.getName());
+
 	@RequestMapping("/listProcesoFormulas")
 	public ModelAndView listConceptos(
 	 ModelMap model, HttpServletRequest request
 	) {
-		log.info("/listProcesoFormulas");
-
-		String user = (String) request.getSession().getAttribute("user");
-
-		if (request.getSession().getAttribute("user") == null) {
-			return new ModelAndView("redirect:/login2");
-		}
-
-		String usuario = (String) request.getSession().getAttribute("user");
-		String idusuario = (String) request.getSession().getAttribute("idUser");
-		String email = (String) request.getSession().getAttribute("email");
-		String firstCharacter = (String) request.getSession().getAttribute("firstCharacter");
-		Integer idCompania = (Integer) request.getSession().getAttribute("idCompania");
-		String nombreComp = (String) request.getSession().getAttribute("nombrecomp");
-		String rucComp = (String) request.getSession().getAttribute("ruccomp");
-		String urlLogo = (String) request.getSession().getAttribute("urlLogo");
-
-		log.info("################### idCompania: " + idCompania);
-
-		model.addAttribute("usuario", usuario);
-		model.addAttribute("idusuario", idusuario);
-		model.addAttribute("email", email);
-		model.addAttribute("firstCharacter", firstCharacter);
-		model.addAttribute("nombreComp", nombreComp);
-		model.addAttribute("rucComp", rucComp);
-		model.addAttribute("idComp", idCompania);
-		model.addAttribute("urlLogo", urlLogo);
-
+		logger.info("/listProcesoFormulas");
+		sessionattributes.getVariablesSession(model, request);
 		List<ProcesoForm> proFosList = service.listProcesoFormula();
-
-		log.info("proFosList: " + proFosList);
-
 		model.addAttribute("proFosList", proFosList);
-
 		return new ModelAndView("public/gladius/confPlanilla/procesosyform/listProcesoFormula");
 	}
 
@@ -66,38 +41,10 @@ public class ProcesoFormulaController {
 	public ModelAndView listConcepto(
 	 ModelMap model, HttpServletRequest request
 	) {
-		log.info("/listConcepto");
-
-		String user = (String) request.getSession().getAttribute("user");
-
-		if (request.getSession().getAttribute("user") == null) {
-			return new ModelAndView("redirect:/login2");
-		}
-
-		String usuario = (String) request.getSession().getAttribute("user");
-		String idusuario = (String) request.getSession().getAttribute("idUser");
-		String email = (String) request.getSession().getAttribute("email");
-		String firstCharacter = (String) request.getSession().getAttribute("firstCharacter");
-		Integer idCompania = (Integer) request.getSession().getAttribute("idCompania");
-		String nombreComp = (String) request.getSession().getAttribute("nombrecomp");
-		String rucComp = (String) request.getSession().getAttribute("ruccomp");
-		String urlLogo = (String) request.getSession().getAttribute("urlLogo");
-
-		log.info("################### idCompania: " + idCompania);
-
-		model.addAttribute("usuario", usuario);
-		model.addAttribute("idusuario", idusuario);
-		model.addAttribute("email", email);
-		model.addAttribute("firstCharacter", firstCharacter);
-		model.addAttribute("nombreComp", nombreComp);
-		model.addAttribute("rucComp", rucComp);
-		model.addAttribute("idComp", idCompania);
-		model.addAttribute("urlLogo", urlLogo);
-
+		logger.info("/listConcepto");
+		sessionattributes.getVariablesSession(model, request);
 		List<Proceso> procesoList = service.listProceso("0");
-
 		model.addAttribute("procesoList", procesoList);
-
 		return new ModelAndView("public/gladius/confPlanilla/procesosyform/listConcepto");
 	}
 
@@ -105,38 +52,10 @@ public class ProcesoFormulaController {
 	public ModelAndView listFormulas(
 	 ModelMap model, HttpServletRequest request
 	) {
-		log.info("/listFormulas");
-
-		String user = (String) request.getSession().getAttribute("user");
-
-		if (request.getSession().getAttribute("user") == null) {
-			return new ModelAndView("redirect:/login2");
-		}
-
-		String usuario = (String) request.getSession().getAttribute("user");
-		String idusuario = (String) request.getSession().getAttribute("idUser");
-		String email = (String) request.getSession().getAttribute("email");
-		String firstCharacter = (String) request.getSession().getAttribute("firstCharacter");
-		Integer idCompania = (Integer) request.getSession().getAttribute("idCompania");
-		String nombreComp = (String) request.getSession().getAttribute("nombrecomp");
-		String rucComp = (String) request.getSession().getAttribute("ruccomp");
-		String urlLogo = (String) request.getSession().getAttribute("urlLogo");
-
-		log.info("################### idCompania: " + idCompania);
-
-		model.addAttribute("usuario", usuario);
-		model.addAttribute("idusuario", idusuario);
-		model.addAttribute("email", email);
-		model.addAttribute("firstCharacter", firstCharacter);
-		model.addAttribute("nombreComp", nombreComp);
-		model.addAttribute("rucComp", rucComp);
-		model.addAttribute("idComp", idCompania);
-		model.addAttribute("urlLogo", urlLogo);
-
+		logger.info("/listFormulas");
+		sessionattributes.getVariablesSession(model, request);
 		List<FormulaXConcepto> formulaXConceptoList = service.listFormulaXConcepto();
-
 		model.addAttribute("formulaXConceptoList", formulaXConceptoList);
-
 		return new ModelAndView("public/gladius/confPlanilla/procesosyform/formulas/listaFormulas");
 	}
 
@@ -144,34 +63,8 @@ public class ProcesoFormulaController {
 	public ModelAndView formular(
 	 ModelMap model, HttpServletRequest request
 	) {
-		log.info("/formularCodigo");
-
-		String user = (String) request.getSession().getAttribute("user");
-
-		if (request.getSession().getAttribute("user") == null) {
-			return new ModelAndView("redirect:/login2");
-		}
-
-		String usuario = (String) request.getSession().getAttribute("user");
-		String idusuario = (String) request.getSession().getAttribute("idUser");
-		String email = (String) request.getSession().getAttribute("email");
-		String firstCharacter = (String) request.getSession().getAttribute("firstCharacter");
-		Integer idCompania = (Integer) request.getSession().getAttribute("idCompania");
-		String nombreComp = (String) request.getSession().getAttribute("nombrecomp");
-		String rucComp = (String) request.getSession().getAttribute("ruccomp");
-		String urlLogo = (String) request.getSession().getAttribute("urlLogo");
-
-		log.info("################### idCompania: " + idCompania);
-
-		model.addAttribute("usuario", usuario);
-		model.addAttribute("idusuario", idusuario);
-		model.addAttribute("email", email);
-		model.addAttribute("firstCharacter", firstCharacter);
-		model.addAttribute("nombreComp", nombreComp);
-		model.addAttribute("rucComp", rucComp);
-		model.addAttribute("idComp", idCompania);
-		model.addAttribute("urlLogo", urlLogo);
-
+		logger.info("/formularCodigo");
+		sessionattributes.getVariablesSession(model, request);
 		return new ModelAndView("public/gladius/confPlanilla/procesosyform/formulas/formularCodigo");
 	}
 }
