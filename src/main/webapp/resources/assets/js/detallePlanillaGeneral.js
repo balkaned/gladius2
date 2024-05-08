d34799d3542401f90ed70efde9134d813b0c38e3
balkaned <gitbalkaned@gmail.com>
2024-05-08 11:44:46 -0500

    Botones Asistencias y modal loading2

--- src/main/webapp/resources/assets/js/detallePlanillaGeneral.js
+++ src/main/webapp/resources/assets/js/detallePlanillaGeneral.js
@@ -1123,5 +1123,5 @@ function reporteAsistencias(){
     var iexcorrel = document.getElementById("iexcorrel").value;
 
     document.getElementById("btnReportAsis").href="actualizarValorTrabConcept@"+iexcodtra+"@"+iexcodpro+"@"+iexperiodo+"@"+iexcodcon+"@1@"+iexcodreg+"@"+valorActualizar;
-    document.getElementById("btnReportAsis").href="AWSorFTP_flgsource@verReportePDF@${idCom}@@null@null@AsistEmpTra@4UP_CODPRO=${iexcodpro}UP_NROPER=${iexperiodo}UP_CODBANK=${xbankproper.codbank}UP_CORREL=1@null@null@null";
+    document.getElementById("btnReportAsis").href="  AWSorFTP_flgsource@verReportePDF@${idCom}@@null@null@AsistEmpTra@4UP_CODPRO=${iexcodpro}UP_NROPER=${iexperiodo}UP_CODBANK=${xbankproper.codbank}UP_CORREL=1@null@null@null";
 }
